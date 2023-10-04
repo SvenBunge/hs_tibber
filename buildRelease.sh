58fc88ed2cade984c6be13fdab6dd990cfc99e1c
Sven Bunge <sven@zeile7.de>
2023-10-04 23:00:40 +0200

    Yeah, Tests first!

--- buildRelease.sh
+++ buildRelease.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 rm release/*
+set -e
+(cd lib/; python2 TestPriceCalc.py)
 (cd ../../; python2 ./generator.pyc hs_tibber utf-8)
 markdown2 --extras tables,fenced-code-blocks,strike,target-blank-links doc/log14464.md > release/log14464.html
 (cd release; zip -r 14464_hs_tibber.hslz *)
